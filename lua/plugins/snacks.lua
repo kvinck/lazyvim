e30c25853c04685d3d5b90c34665b6aae4720af8
Kevin Vinck <kevin@lastedit.com>
2026-09-13 09:01:40 -0400

    Show hidden files when browsing and finding dotfiles

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,11 @@ return {
     "snacks.nvim",
     opts = {
         dim = { enabled = true },
+        picker = {
+            sources = {
+                explorer = { hidden = true },
+                files = { hidden = true },
+            },
+        },
     },
 }
